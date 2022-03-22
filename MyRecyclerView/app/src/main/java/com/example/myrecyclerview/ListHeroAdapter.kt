b53aa010c340eb3170f6a3fee2d289a7a3a77baf
IbramBachri <95865714+IbramBachri@users.noreply.github.com>
2022-03-22 19:37:55 +0700

    history learning

---
+++ MyRecyclerView/app/src/main/java/com/example/myrecyclerview/ListHeroAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.myrecyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class ListHeroAdapter(private val listhero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val (name, description, photo) = listhero[position]
+        Glide.with(holder.itemView.context)
+            .load(photo) // URL Gambar
+            .circleCrop() // Mengubah image menjadi lingkaran
+            .into(holder.imgPhoto) // imageView mana yang akan diterapkan
+        holder.tvName.text = name
+        holder.tvDescription.text = description
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(listhero[holder.adapterPosition]) }
+    }
+
+    override fun getItemCount(): Int = listhero.size
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+        var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+        var tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
+    }
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Hero)
+    }
+}
